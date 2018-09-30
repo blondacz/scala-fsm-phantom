99d05339d2b6b790d195f2ca5a85ee4a3ba9ce30
blondacz <tomas.klubal@gmail.com>
2018-09-30 22:17:43 +0100

    Removed println

--- src/main/scala/io/generators/fsm/instructions/Instruction.scala
+++ src/main/scala/io/generators/fsm/instructions/Instruction.scala
@@ -8,27 +8,27 @@ import io.generators.fsm.instructions.Instruction.InstructionState.Marker._
 class Instruction[S <: InstructionState] {
 
   def publish[T >: S <: New]: Instruction[Published] = {
-    println("published")
     new Instruction
   }
 
   def acknowledge[T >: S <: Acknowledgeable] : Instruction[Instructed] = {
-    println("acked")
     new Instruction
   }
 
   def cancel[T >: S <: Cancellable] : Instruction[CancelSubmitted] = {
-    println("cancel submitted")
     new Instruction
   }
 
   def nack[T >: S <: Acknowledgeable] : Instruction[Failed] = {
-    println("nacked")
     new Instruction
   }
 }
 
 object Instruction {
+
+  implicit class transitionOps[T](value: T){
+    def ~>[B](f: T => B): B = f(value)
+  }
 
   sealed trait InstructionState
   object InstructionState {
@@ -50,7 +50,6 @@ object Instruction {
 
 object instructing extends App {
 
-  println("\nFirst instruction flow---")
   val i = new Instruction[New]
   val i2: Instruction[Published] = i.publish
   val i3: Instruction[Instructed] = i2.acknowledge
@@ -58,10 +57,8 @@ object instructing extends App {
   val i5 = i2.acknowledge
 
 
-  println("\nSecond instruction flow---")
   val j = new Instruction[New]
   val j2 : Instruction[Published] = j.publish
   val j3 : Instruction[Failed] = j.nack
   val j4  = j.cancel
-
 }
