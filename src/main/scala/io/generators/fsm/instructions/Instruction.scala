81559e9001a3225b57141c8b94a45619d6bea25b
blondacz <tomas.klubal@gmail.com>
2018-09-30 23:28:24 +0100

    Added Instruction reference as property

--- src/main/scala/io/generators/fsm/instructions/Instruction.scala
+++ src/main/scala/io/generators/fsm/instructions/Instruction.scala
@@ -5,23 +5,13 @@ import io.generators.fsm.instructions.Instruction.InstructionState._
 import io.generators.fsm.instructions.Instruction.InstructionState.Marker._
 
 
-class Instruction[S <: InstructionState] {
+case class Instruction[S <: InstructionState](ref: String) {
 
-  def publish[T >: S <: New]: Instruction[Published] = {
-    new Instruction
-  }
-
-  def acknowledge[T >: S <: Acknowledgeable] : Instruction[Instructed] = {
-    new Instruction
-  }
-
-  def cancel[T >: S <: Cancellable] : Instruction[CancelSubmitted] = {
-    new Instruction
-  }
-
-  def nack[T >: S <: Acknowledgeable] : Instruction[Failed] = {
-    new Instruction
-  }
+  def failGeneration[T >: S <: New]: Instruction[Failed] = this.copy()
+  def publish[T >: S <: New]: Instruction[Published] = this.copy()
+  def acknowledge[T >: S <: Acknowledgeable] : Instruction[Instructed] = this.copy()
+  def cancel[T >: S <: Cancellable] : Instruction[CancelSubmitted] = this.copy()
+  def nack[T >: S <: Acknowledgeable] : Instruction[Failed] = this.copy()
 }
 
 object Instruction {
@@ -35,14 +25,14 @@ object Instruction {
     sealed trait New extends InstructionState
     sealed trait Published extends Cancellable with Acknowledgeable
     sealed trait Instructed extends Cancellable
-    sealed trait Cancelled extends Terminated
+    sealed trait Cancelled extends Final
     sealed trait CancelSubmitted extends Acknowledgeable
     sealed trait Failed extends Cancellable
-    sealed trait NotInstructed extends Terminated
+    sealed trait NotInstructed extends Final
     //marker states
     object Marker {
       sealed trait Cancellable extends InstructionState
-      sealed trait Terminated extends InstructionState
+      sealed trait Final extends InstructionState
       sealed trait Acknowledgeable extends InstructionState
     }
   }
@@ -50,14 +40,14 @@ object Instruction {
 
 object instructing extends App {
 
-  val i = new Instruction[New]
+  val i = new Instruction[New]("aRef")
   val i2: Instruction[Published] = i.publish
   val i3: Instruction[Instructed] = i2.acknowledge
   val i4: Instruction[CancelSubmitted] = i2.cancel
   val i5 = i2.acknowledge
 
 
-  val j = new Instruction[New]
+  val j = new Instruction[New]("bRef")
   val j2 : Instruction[Published] = j.publish
   val j3 : Instruction[Failed] = j.nack
   val j4  = j.cancel
